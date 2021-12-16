65e34076c26e5c9d49b87d3f21a958f22dee9dff
sofiane dardek <sofiane.dardek@hotmail.fr>
2021-12-16 15:57:22 +0100

    Amelioration du code
    - ajout du package symfony/var-dumper
    - ajout de code qui dump

---
+++ var-dump.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+var_dump(1 == '1');
+
+
+var_dump(1 === '1');
+
+
+
+$text = 'foo bar baz';
+dump($text);
+
+
+$number = 1;
+dump($number);
+
+
+$number2 = 2.5; 
+dump($number2);
+
+$null = null;
+dump($null);
