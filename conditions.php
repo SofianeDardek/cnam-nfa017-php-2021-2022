f8cb4d20ab1de1e646d3f019336407257c814ac5
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-06 10:54:01 +0100

    Amélioration du code
    - ajout de code de conditions

--- conditions.php
+++ conditions.php
@@ -1,4 +1,6 @@
 <?php
+// 1 = true
+// 0 = false 
 require __DIR__.'/vendor/autoload.php';
 
 $isWaterOff = (bool) random_int(0, 1);
@@ -22,4 +24,39 @@ if($isWaterOff && $isGasOff){
 }
 
 
+$hasPaymentCard = (bool) random_int(0,1);
+$hasCash = (bool) random_int(0,1);
 
+dump($hasPaymentCard);
+dump($hasCash);
+
+if($hasPaymentCard || $hasCash){
+    echo "Vous pouvez partir faire les courses<br>\n";
+
+    if($hasPaymentCard == false){
+        echo "il manque la carte de paiement<br>\n";
+    }
+
+    if($hasCash == false){
+        echo "il manque le liquide<br>\n";
+    }
+} else {
+    echo "Vous ne pouvez pas faire la course<br>\n";
+    echo " Vous n'avez aucun moyen de paiement<br>\n";
+
+}
+
+
+$stock = random_int(0, 10);
+
+dump($stock);
+
+if($stock == 0 ){
+    echo "rupture de stock<br>\n";
+} elseif($stock == 1){
+    echo "il reste $stock piéce<br>\n";
+} elseif($stock <=3){
+    echo "il reste $stock pièces<br>\n";
+} else {
+    echo "stock disponible";
+}
