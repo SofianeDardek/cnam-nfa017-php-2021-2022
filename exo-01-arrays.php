9a8e9c1163a6e18d6103a7009938e70ed8e002f3
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-07 10:57:11 +0100

    Ajout exercice jeu de carte
    Ajout de code

---
+++ exo-01-arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
+
+Exemple de nom de cartes : 'As pique', '3 carreau', 'Valet treffle', etc.
+
+Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus.
+
+À l'aide de ce nombre, retirer la "n-ième" carte de la main.
+
+Affichez le nom de la carte qui a été retirée.
+*/
+
+
+$cartes = [
+    'As pique',
+    '3 carreau',
+    'Valet treffle',
+    '3 coeur',
+    '5 pique'
+];
+
+$number = random_int(0, 4);
+
+echo "Vous avez tiré la carte " . "<b>n°". "$number " . $cartes[$number] . "</b>";
