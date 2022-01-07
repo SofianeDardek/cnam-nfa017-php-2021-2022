7000db1e91242edaddb8576c146fc67d3dd13045
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-07 15:02:28 +0100

    Ajout de code
    - Ajout de conditions

--- exo-02-arrays.php
+++ exo-02-arrays.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
 /*
 Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
 
@@ -26,3 +28,56 @@ Affichez les nombres tirés au hasard et le nom des cartes qui ont été retiré
 Comparez les valeurs numériques des cartes et affichez quelle carte a une valeur supérieur ou s'il y a égalité.
 Attention, par convention la carte "as" (valeur 1) est la plus élevée.
 */
+
+$cartes = [
+    '6 coeur',
+    '3 carreau',
+    '4 treffle',
+    '3 coeur',
+    '5 pique'
+];
+
+
+$tailleTab = count($cartes);
+
+$tirage1 = random_int(1, $tailleTab-1);
+$tirage2 = random_int(1, $tailleTab-1);
+
+$tirages = [];
+
+array_push($tirages, $tirage1, $tirage2);
+
+if($tirage1 == $tirage2){ echo 'Vous avez tiré le même chiffre'; exit();}
+
+$carte1 = array_splice($cartes, $tirage1-1, 1);
+$carte2 = array_splice($cartes, $tirage1-1, 1);
+
+$carte1 = $carte1[0];
+$carte2 = $carte2[0];
+echo "Vous avez tiré la carte $carte1 pour la premiere carte <br>";
+echo "Vous avez tiré la carte $carte2 pour la deuxieme carte";
+
+
+dump($carte1);
+dump($carte2);
+
+$valCarte1 = (int) $carte1[0];
+$valCarte2 = (int) $carte2[0];
+
+
+if($valCarte1 > $valCarte2 || $valCarte1 == 1){
+    echo "La carte $carte1 est supérieur à la carte $carte2";
+} elseif($valCarte2 < $valCarte1 || $valCarte2 == 1){
+    echo "La carte $carte1 est supérieur à la carte $carte2";
+} elseif($valCarte1 == $valCarte2){
+    echo "la carte $carte1 et $carte2 sont équivalentes";
+} 
+
+
+
+dump($valCarte1);
+dump($valCarte2);
+
+
+
+dump($tirages);
