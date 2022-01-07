8d48ae8c7e7efe15fbe1f1a8c9a69ac0ab055f22
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-07 12:18:08 +0100

    Création  d'un nouveau fichier
    - Exercice sur les tableaux

---
+++ exo-02-arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
+
+Exemple de nom de cartes : '1 pique', '3 carreau', '11 treffle', etc.
+
+Rappel des valeurs de certaines cartes :
+
+- as : 1
+- valet : 11
+- dame : 12
+- roi : 13
+
+Stockez la taille du tableau dans une variable.
+
+Tirez deux nombres au hasard, compris entre 1 et la taille du tableau inclus, en utilisant la fonction random_int().
+Stockez ces nombres dans un tableau.
+
+Si le même nombre a été tiré deux fois, affichez un message qui dit que le même nombre a été tiré, puis arrêtez le programme avec la fonction exit().
+
+À l'aide de ces nombres, retirer deux cartes de la main.
+
+Affichez les nombres tirés au hasard et le nom des cartes qui ont été retirées.
+
+Comparez les valeurs numériques des cartes et affichez quelle carte a une valeur supérieur ou s'il y a égalité.
+Attention, par convention la carte "as" (valeur 1) est la plus élevée.
+*/
