38128ac4cfad4c16905bbe0e497a362892fc69a5
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-13 12:22:06 +0100

    Modification
    - Ajout de code

--- loops.php
+++ loops.php
@@ -3,3 +3,20 @@
 for($i = 0; $i < 10; $i++){
     echo "$i<br>\n";
 }
+
+echo '<br>';
+
+$fruits = ['ananas', 'banane', 'cerise'];
+
+for($i = 0; $i < count($fruits); $i++){
+    echo "$fruits[$i]<br>\n";
+}
+
+echo '<br>';
+
+foreach($fruits as $key => $fruit){
+    echo "$key : $fruit";
+}
+
+echo '<br>';
+
